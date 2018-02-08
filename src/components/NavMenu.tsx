9bc89e5065caa906bb898b13164783f0432be8c5
Yang Wang <luanshixia@gmail.com>
2018-02-08 00:34:47 -0800

    Clean up

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -13,7 +13,7 @@ export class NavMenu extends React.Component<{}, {}> {
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-          <Link className="navbar-brand" to={'/'}>Redux101</Link>
+          <Link className="navbar-brand" to={'/'}>eMind</Link>
         </div>
         <div className="clearfix"></div>
         <div className="navbar-collapse collapse">
@@ -24,13 +24,13 @@ export class NavMenu extends React.Component<{}, {}> {
               </NavLink>
             </li>
             <li>
-              <NavLink to={'/counter'} activeClassName="active">
-                <span className="glyphicon glyphicon-education"></span> Counter
+              <NavLink to={'/help'} activeClassName="active">
+                <span className="glyphicon glyphicon-education"></span> Help
               </NavLink>
             </li>
             <li>
-              <NavLink to={'/fetchdata'} activeClassName="active">
-                <span className="glyphicon glyphicon-th-list"></span> Fetch data
+              <NavLink to={'/about'} activeClassName="active">
+                <span className="glyphicon glyphicon-th-list"></span> About
               </NavLink>
             </li>
           </ul>
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import Home from './components/Home';
-import FetchData from './components/FetchData';
-import Counter from './components/Counter';
+import Help from './components/Help';
+import About from './components/About';
 
 export const routes = (
 <Layout>
   <Route exact path="/" component={Home} />
-  <Route path="/counter" component={Counter} />
-  <Route path="/fetchdata/:startDateIndex?" component={FetchData} />
+  <Route path="/help" component={Help} />
+  <Route path="/about" component={About} />
 </Layout>
 );
